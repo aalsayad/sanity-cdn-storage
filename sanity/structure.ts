4531a18a7f967613dcac854903300d8701b66859
Ahmed Alsayad <aal_sayad@outlook.com>
2025-05-03 14:17:35 +0300

    newer structure for Sanity

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,6 +1,5 @@
 // sanity/structure.ts
-
-import { StructureResolver } from "sanity/desk";
+import { StructureResolver } from "sanity/structure"; // Updated import
 
 export const structure: StructureResolver = (S) =>
   S.list().title("Content").items(S.documentTypeListItems());
